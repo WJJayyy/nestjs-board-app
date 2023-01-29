83d89674812b0e26f82bd041756d386bd4c85be7
WJJayyy <91gjstr@gmail.com>
2023-01-29 23:29:19 +0900

    refactor: 모든 게시물 가져오기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -20,6 +20,10 @@ export class BoardsService {
     //         return this.boards
     //     }
 
+    async getAllBoards(): Promise<Board[]> {
+        return this.boardRepository.find();
+    }
+
     //     createBoard(createBoardDto: CreateBoardDto) {
     //         const { title, description } = createBoardDto
     //         const board: Board = {
